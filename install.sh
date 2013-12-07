0cf2226da7de15e6dc3708386ea19c120f57118f
Paul Engel <paul.engel@holder.nl>
2013-12-07 04:04:04 +0100

    Improved install and uninstall scripts

--- install.sh
+++ install.sh
@@ -1,12 +1,22 @@
-echo "Downloading https://github.com/archan937/jsonv.sh/archive/0.1.0.tar.gz"
-curl -L -s -o jsonv.sh-0.1.0.tar.gz https://github.com/archan937/jsonv.sh/archive/0.1.0.tar.gz
-echo "Unpacking jsonv.tar.gz"
-tar xzf jsonv.sh-0.1.0.tar.gz
+version="0.1.0"
+
+echo "Downloading https://github.com/archan937/jsonv.sh/archive/$version.tar.gz"
+curl -L -s https://github.com/archan937/jsonv.sh/archive/$version.tar.gz -o jsonv.sh-$version.tar.gz
+
+echo "Unpacking jsonv.sh-$version.tar.gz"
+tar xzf jsonv.sh-$version.tar.gz
+
 echo "Installing jsonv"
-cd jsonv.sh-0.1.0
+cd jsonv.sh-$version
 make install
 cd ..
+
 echo "Removing installation files"
-rm jsonv.sh-0.1.0.tar.gz
-rm -rf jsonv.sh-0.1.0
+rm jsonv.sh-$version.tar.gz
+rm -rf jsonv.sh-$version
+
+if ! type "gawk" > /dev/null 2>&1; then
+  echo 'WARNING: Make sure to have `gawk` installed in order to make `jsonv` work'
+fi
+
 echo "Done."
--- uninstall.sh
+++ uninstall.sh
@@ -1,7 +1,12 @@
+makefile="Makefile.jsonv"
+
 echo "Downloading https://raw.github.com/archan937/jsonv.sh/master/Makefile"
-curl -L -s -o Makefile https://raw.github.com/archan937/jsonv.sh/master/Makefile
+curl -L -s https://raw.github.com/archan937/jsonv.sh/master/Makefile -o $makefile
+
 echo "Uninstalling jsonv"
-make uninstall
+make uninstall --makefile $makefile
+
 echo "Removing Makefile"
-rm Makefile
+rm $makefile
+
 echo "Done."
